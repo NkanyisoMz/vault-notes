9321d960abc9f826cf5d9ada4cdc49cc92f31ec8
Nkanyiso Mzobe <104601010+NkanyisoMz@users.noreply.github.com>
2026-06-23 16:58:17 +0200

    Implement note creation for authenticated users

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -4,4 +4,24 @@ class NotesController < ApplicationController
     def index
         @notes = current_user.notes
     end
+
+    def new
+        @note = Note.new
+    end
+
+    def create
+        @note = current_user.notes.build(note_params)
+
+        if @note.save
+            redirect_to notes_path
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def note_params
+        params.require(:note).permit(:title, :content)
+    end
 end
